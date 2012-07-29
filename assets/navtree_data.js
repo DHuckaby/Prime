87da3fb4ee5082b414d234ca7e7654256eb5cef6
Daniel Huckaby <dhuckaby@mercury.io>
2012-07-29 12:50:23 -0500

    Updated javadocs

--- assets/navtree_data.js
+++ assets/navtree_data.js
@@ -5,7 +5,7 @@ var NAVTREE_DATA =
 , "" ], [ "com.handlerexploit.prime.drawable", "com/handlerexploit/prime/drawable/package-summary.html", [ [ "Classes", null, [ [ "RemoteStateListDrawable", "com/handlerexploit/prime/drawable/RemoteStateListDrawable.html", null, "" ] ]
 , "" ] ]
 , "" ], [ "com.handlerexploit.prime.utils", "com/handlerexploit/prime/utils/package-summary.html", [ [ "Interfaces", null, [ [ "ImageManager.ExtendedRequest", "com/handlerexploit/prime/utils/ImageManager.ExtendedRequest.html", null, "" ], [ "ImageManager.OnImageReceivedListener", "com/handlerexploit/prime/utils/ImageManager.OnImageReceivedListener.html", null, "" ], [ "ImageManager.Request", "com/handlerexploit/prime/utils/ImageManager.Request.html", null, "" ] ]
-, "" ], [ "Classes", null, [ [ "ImageManager", "com/handlerexploit/prime/utils/ImageManager.html", null, "" ], [ "ImageManager.LowPriorityThreadFactory", "com/handlerexploit/prime/utils/ImageManager.LowPriorityThreadFactory.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "ImageManager", "com/handlerexploit/prime/utils/ImageManager.html", null, "" ] ]
 , "" ] ]
 , "" ], [ "com.handlerexploit.prime.widgets", "com/handlerexploit/prime/widgets/package-summary.html", [ [ "Classes", null, [ [ "RemoteImageView", "com/handlerexploit/prime/widgets/RemoteImageView.html", null, "" ] ]
 , "" ] ]
--- reference/lists.js
+++ reference/lists.js
@@ -7,10 +7,9 @@ var DATA = [
       { id:5, label:"com.handlerexploit.prime.utils", link:"com/handlerexploit/prime/utils/package-summary.html", type:"package" },
       { id:6, label:"com.handlerexploit.prime.utils.ImageManager", link:"com/handlerexploit/prime/utils/ImageManager.html", type:"class" },
       { id:7, label:"com.handlerexploit.prime.utils.ImageManager.ExtendedRequest", link:"com/handlerexploit/prime/utils/ImageManager.ExtendedRequest.html", type:"class" },
-      { id:8, label:"com.handlerexploit.prime.utils.ImageManager.LowPriorityThreadFactory", link:"com/handlerexploit/prime/utils/ImageManager.LowPriorityThreadFactory.html", type:"class" },
-      { id:9, label:"com.handlerexploit.prime.utils.ImageManager.OnImageReceivedListener", link:"com/handlerexploit/prime/utils/ImageManager.OnImageReceivedListener.html", type:"class" },
-      { id:10, label:"com.handlerexploit.prime.utils.ImageManager.Request", link:"com/handlerexploit/prime/utils/ImageManager.Request.html", type:"class" },
-      { id:11, label:"com.handlerexploit.prime.widgets", link:"com/handlerexploit/prime/widgets/package-summary.html", type:"package" },
-      { id:12, label:"com.handlerexploit.prime.widgets.RemoteImageView", link:"com/handlerexploit/prime/widgets/RemoteImageView.html", type:"class" }
+      { id:8, label:"com.handlerexploit.prime.utils.ImageManager.OnImageReceivedListener", link:"com/handlerexploit/prime/utils/ImageManager.OnImageReceivedListener.html", type:"class" },
+      { id:9, label:"com.handlerexploit.prime.utils.ImageManager.Request", link:"com/handlerexploit/prime/utils/ImageManager.Request.html", type:"class" },
+      { id:10, label:"com.handlerexploit.prime.widgets", link:"com/handlerexploit/prime/widgets/package-summary.html", type:"package" },
+      { id:11, label:"com.handlerexploit.prime.widgets.RemoteImageView", link:"com/handlerexploit/prime/widgets/RemoteImageView.html", type:"class" }
 
     ];
