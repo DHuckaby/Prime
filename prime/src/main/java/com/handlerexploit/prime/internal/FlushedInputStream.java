a223b8ca9288dab534424e44efad4bc5cbe532a7
Daniel Huckaby <dhuckaby@mercury.io>
2013-11-18 21:24:54 -0600

    1.0 API Snapshot

---
+++ prime/src/main/java/com/handlerexploit/prime/internal/FlushedInputStream.java
@@ -0,0 +1,30 @@
+package com.handlerexploit.prime.internal;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FlushedInputStream extends FilterInputStream {
+
+    public FlushedInputStream(InputStream inputStream) {
+        super(inputStream);
+    }
+
+    @Override
+    public long skip(long n) throws IOException {
+        long totalBytesSkipped = 0L;
+        while (totalBytesSkipped < n) {
+            long bytesSkipped = in.skip(n - totalBytesSkipped);
+            if (bytesSkipped == 0L) {
+                int b = read();
+                if (b < 0) {
+                    break;  // we reached EOF
+                } else {
+                    bytesSkipped = 1; // we read one byte
+                }
+            }
+            totalBytesSkipped += bytesSkipped;
+        }
+        return totalBytesSkipped;
+    }
+}
